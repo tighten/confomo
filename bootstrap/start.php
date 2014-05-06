2fa4d3cf31e9ce2e57b1e9333222f494d19fc378
Matt Stauffer <mstauffer@gmail.com>
2014-05-06 18:11:39 -0400

    Update config for local testing

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-));
+$env = $app->detectEnvironment(function()
+{
+    return getenv('M347_ENV') ? getenv('M347_ENV') : 'production';
+});
 
 /*
 |--------------------------------------------------------------------------
