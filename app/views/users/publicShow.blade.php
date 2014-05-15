5f42ad8767a61f62ba49f6ee52d2d09bdbb66bf2
Matt Stauffer <mstauffer@gmail.com>
2014-05-15 08:19:37 -0400

    Add profile pics to public

--- app/views/users/publicShow.blade.php
+++ app/views/users/publicShow.blade.php
@@ -6,13 +6,13 @@
 	<div id="old-friends" data-type="old">
 		<h3>Old friends to meet at conf</h3>
 		<i>(Grey = met)</i>
-		<ul>
+		<ul class="public-list">
 			@foreach($user->oldFriends as $friend)
-			<li class="{{ $friend->met ? 'public-mark-as-met' : '' }}"><a href="http://twitter.com/{{ $friend->twitter }}">{{ '@' . $friend->twitter }}</a></li>
+			<li style="clear: both;" class="{{ $friend->met ? 'public-mark-as-met' : '' }}"><img src="/{{ $friend->twitter_profile_pic }}" class="friend-list-item__image"><a href="http://twitter.com/{{ $friend->twitter }}">{{ '@' . $friend->twitter }}</a></li>
 			@endforeach
 		</ul>
 
-		<form data-bind="submit: addItem">
+		<form data-bind="submit: addItem" style="clear: both; padding-top: 1rem;">
 			<label for="suggestOldFriend">Suggest old friend for `{{ $user->username }}` to meet:</label><br>
 			@<input data-bind='value: itemToAdd, valueUpdate: "afterkeydown"' type="text" autocorrect="off" autocapitalize="off">
 			<button type="submit" data-bind="enable: itemToAdd().length > 0">Add</button>
@@ -22,13 +22,13 @@
 
 	<div id="new-friends" data-type="new">
 		<h3>New friends met at conf</h3>
-		<ul>
+		<ul class="public-list">
 			@foreach($user->newFriends as $friend)
-			<li><a href="http://twitter.com/{{ $friend->twitter }}">{{ '@' . $friend->twitter }}</a></li>
+			<li style="clear: both;" class="{{ $friend->met ? 'public-mark-as-met' : '' }}"><img src="/{{ $friend->twitter_profile_pic }}" class="friend-list-item__image"><a href="http://twitter.com/{{ $friend->twitter }}">{{ '@' . $friend->twitter }}</a></li>
 			@endforeach
 		</ul>
 
-		<form data-bind="submit: addItem">
+		<form data-bind="submit: addItem" style="clear: both; padding-top: 1rem;">
 			<label for="suggestOldFriend">Suggest new friend for `{{ $user->username }}` to track as "met":</label><br>
 			@<input data-bind='value: itemToAdd, valueUpdate: "afterkeydown"' type="text" autocorrect="off" autocapitalize="off">
 			<button type="submit" data-bind="enable: itemToAdd().length > 0">Add</button>
