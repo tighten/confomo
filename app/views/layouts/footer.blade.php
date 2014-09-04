255a9e7523cba68e178fe92f4fd317828cfb0bc7
Matt Stauffer <mstauffer@gmail.com>
2014-09-04 09:52:33 -0400

    Delete todos from views. #1

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,7 +1,3 @@
-<div class="footer">
-{{ HTML::linkAction('todo', 'To do') }}
-</div>
-
 <script src="/bower_components/jquery/dist/jquery.js"></script>
 <script src="/bower_components/component-knockout-passy/knockout.js"></script>
 <script src="/assets/js/{{ $context }}.js"></script>
