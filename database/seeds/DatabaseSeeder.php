afe9af1fece3c007e5d48f035b8e35e84061940e
Matt Stauffer <matt@tighten.co>
2015-01-23 21:14:14 -0500

    Fix seeder user/alias

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,17 +1,20 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Seeder;
+
 class DatabaseSeeder extends Seeder
 {
     /**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
         Eloquent::unguard();
 
-         $this->call('FriendsTableSeeder');
+        $this->call('FriendsTableSeeder');
     }
 
 }
--- database/seeds/FriendsTableSeeder.php
+++ database/seeds/FriendsTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 // Composer: "fzaninotto/faker": "v1.3.0"
+use Confomo\Entities\Friend;
 use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
 
 class FriendsTableSeeder extends Seeder
 {
@@ -9,7 +11,7 @@ class FriendsTableSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        foreach(range(1, 10) as $index) {
+        foreach (range(1, 10) as $index) {
             Friend::create([
                 'first_name' => $faker->firstName,
                 'last_name' => $faker->lastName,
