2ab7acad2e7d72ba5d9af34105ee9346b27a24c7
Michael Dyrynda <michael@dyrynda.com.au>
2016-06-13 14:44:54 +0930

    Add a fileResponse method (was added in 5.2)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function fileResponse($path, $headers = [])
+    {
+        return new BinaryFileResponse($path, 200, $headers);
+    }
 }
