41fc0738cd3f640faf6054a5a2a28cb1df355e3e
Michael Dyrynda <michael@dyrynda.com.au>
2016-06-13 14:44:27 +0930

    Extract fetching of Twitter avatar

--- app/Friend.php
+++ app/Friend.php
@@ -9,50 +9,19 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 class Friend extends Model
 {
     protected $fillable = ['username', 'avatar', 'type', 'met'];
+    protected $appends = ['avatar_url'];
     protected $casts = [
         'met' => 'boolean',
     ];
 
-    public static function boot()
+    public function getAvatarAttribute()
     {
-        parent::boot();
-
-        /**
-         * When deleting a friend, if they are the last record in the database,
-         * they have an avatar set, and they are the last record with this
-         * username, also remove the cached twitter avatar from storage.
-         */
-        static::deleted(function ($friend) {
-            if (static::where('username', $friend->username)->count() == 0) {
-                if ($friend->avatar && file_exists($avatar_path = public_path($friend->avatar))) {
-                    @unlink($avatar_path);
-                }
-            }
-        });
+        return sprintf('assets/img/cache/twitter_profile_pics/%s', sha1($this->username));
     }
 
-    public function fetchAvatar()
+    public function getAvatarUrlAttribute()
     {
-        // If the friend already exists, use the existing avatar
-        if ($friend = static::where('username', $this->username)->whereNotNull('avatar')->first()) {
-            return $this->update(['avatar' => $friend->avatar]);
-        }
-
-        try {
-            $twitter = app(TwitterOAuth::class);
-            $details = $twitter->get('users/show', ['screen_name' => $this->username]);
-
-            $url = str_replace('_normal', '', $details->profile_image_url_https);
-            $extension = pathinfo($url, PATHINFO_EXTENSION);
-            $path = sprintf('assets/img/cache/twitter_profile_pics/%s.%s', $this->username, $extension);
-
-            if (@file_put_contents(public_path($path), @file_get_contents($url))) {
-                return $this->update(['avatar' => $path]);
-            }
-        } catch (Exception $e) {
-            // No big deal
-            return false;
-        }
+        return asset(sprintf('avatar/%s', $this->username));
     }
 
     public function markMet()
