1e26aba37beee9bf87586354bd5b0a2075853b70
Michael Dyrynda <michael@dyrynda.com.au>
2016-07-16 22:16:24 +0930

    Add method to store a friend introduction

--- app/Http/Controllers/ConferenceIntroductionController.php
+++ app/Http/Controllers/ConferenceIntroductionController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Conference;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ConferenceIntroductionController extends Controller
 {
     public function index(Conference $conference)
     {
         return view('conferences.introduce')->with('conference', $conference);
+    }
+
+    public function store(Conference $conference, Request $request)
+    {
+        return $conference->makeIntroduction($request->input('username'));
     }
 }
