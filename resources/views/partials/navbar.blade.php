cf5e7c4ae8ef4ed50ee3782fa8827f27dbb574b8
Matt Stauffer <matt@tighten.co>
2016-07-19 17:09:38 -0400

    Move some styles into css file; drop logo width for iPhone 6

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,18 +1,6 @@
-<style>
-    /* Temporary. I hate Bootstrap so badly. -MES */
-    .nav-action-button {
-        margin-top: 0.75em;
-    }
-
-    @media only screen and (min-width: 600px) {
-        .nav-action-button {
-            margin-top: 2em;
-        }
-    }
-</style>
 <nav class="navbar navbar-default">
     <div class="container">
-        <div style="float: right; min-width: 200px; text-align: right;">
+        <div class="nav-buttons">
             @if (App::environment('local') && Auth::guest())
                 <a href="/local-login" class="btn btn-default nav-action-button">
                     Login Magically
@@ -30,7 +18,10 @@
         </div>
 
         <a href="/">
-            <img src="{{ asset('assets/img/confomo-logo.png') }}" alt="ConFOMO | Never miss out on new friends" style="max-width: 45%;">
+            <img
+                src="{{ asset('assets/img/confomo-logo.png') }}"
+                alt="ConFOMO | Never miss out on new friends"
+                class="logo">
         </a>
     </div>
 </nav>
