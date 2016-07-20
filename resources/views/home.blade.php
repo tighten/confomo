123f1eaf81e5a186bfe94b56460599d52c0f6495
Michael Dyrynda <michael@dyrynda.com.au>
2016-07-20 19:56:26 +0930

    Remove inline styles, add responsive styles for landing page
    Dial the size down a little bit on mobile devices so that the screenshot
    is visible

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container landing">
         <div class="row">
             <div class="col-sm-12">
                 <h1>Welcome to ConFOMO!</h1>
 
-                <p style="font-size: 2em; margin-bottom: 1em;">ConFOMO is a simple tool that makes it easy to track your <strong>friends</strong> at <strong>conferences</strong>.</p>
+                <p>ConFOMO is a simple tool that makes it easy to track your <strong>friends</strong> at <strong>conferences</strong>.</p>
 
-                <p style="font-size: 2em; margin-bottom: 1em;">With ConFOMO, <strong>you don't have to fear missing out</strong>; you can make a plan of friends you <em>want to meet</em> at a conference you're going to attend, track <em>people you met at the conference</em>, and even let your friends (new or old) "introduce" themselves to your ConFOMO list.</p>
+                <p>With ConFOMO, <strong>you don't have to fear missing out</strong>; you can make a plan of friends you <em>want to meet</em> at a conference you're going to attend, track <em>people you met at the conference</em>, and even let your friends (new or old) "introduce" themselves to your ConFOMO list.</p>
 
-                <p style="font-size: 2em;">Sign up for free to try it now. Just hit "Login With Twitter" above.</p>
+                <p>Sign up for free to try it now. Just hit "Login With Twitter" above.</p>
 
                 <img src="/assets/img/confomo-screenshot.png" alt="ConFOMO screenshot" style="border: 0.5em solid #eee; width: 800px; margin: 0 auto; display: block; margin-top: 3em;">
             </div>
