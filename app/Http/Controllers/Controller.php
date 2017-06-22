798020a941fd88c3789fb9dd6afcdb53fbc2a7fa
Laravel Shift <shift@laravelshift.com>
2017-06-22 19:00:26 +0000

    Shift core files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 
     /**
      * Return a file response.
