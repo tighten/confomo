1dcabf364c3b2feafdad43d1d2c56b882a906e34
Matt Stauffer <matt@tighten.co>
2017-07-05 23:15:22 -0400

    Schedule twitter pic sync

--- app/Jobs/FetchTwitterAvatar.php
+++ app/Jobs/FetchTwitterAvatar.php
@@ -6,6 +6,7 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 use App\Friend;
 use Exception;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Support\Facades\Log;
 
 class FetchTwitterAvatar extends Job implements SelfHandling
 {
@@ -37,9 +38,11 @@ class FetchTwitterAvatar extends Job implements SelfHandling
             $url = str_replace('_normal', '', $details->profile_image_url_https);
 
             if (@file_put_contents(public_path($this->friend->avatar), @file_get_contents($url))) {
+                Log::info('Synced avatar for ' . $this->friend->username);
                 return true;
             }
         } catch (Exception $e) {
+            Log::error('Failed syncing avatar for ' . $this->friend->username);
             // No big deal, as we have a default avatar
         }
 
