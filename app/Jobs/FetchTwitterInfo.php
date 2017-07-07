c2a9138d8029a327edf6d67efac21f6b1c516ab1
Matt Stauffer <matt@tighten.co>
2017-07-06 23:14:16 -0400

    Update fetch twitter info with latest changes to fetch avatar

--- app/Jobs/FetchTwitterInfo.php
+++ app/Jobs/FetchTwitterInfo.php
@@ -30,30 +30,35 @@ class FetchTwitterInfo extends Job implements SelfHandling
      *
      * @return void
      */
-    public function handle()
+    public function handle(TwitterOAuth $twitter)
     {
         try {
-            $details = app(TwitterOAuth::class)->get('users/show', ['screen_name' => $this->friend->username]);
+            $details = $twitter->get('users/show', ['screen_name' => $this->friend->username]);
+
             $this->friend->name = $details->name;
             $this->friend->location = $details->location;
+            $this->friend->description = $details->description;
 
             if (array_has($details->entities, 'url')) {
                 $this->friend->url = $details->url;
                 $this->friend->url_display = $details->entities->url->urls[0]->display_url;
             }
 
-            $this->friend->description = $details->description;
             $this->friend->save();
+            Log::info('Synced friend details for ' . $this->friend->username);
 
             $avatar_url = str_replace('_normal', '', $details->profile_image_url_https);
 
             if (@file_put_contents(public_path($this->friend->avatar), @file_get_contents($avatar_url))) {
+                Log::info('Synced avatar for ' . $this->friend->username);
                 return true;
             }
         } catch (Exception $e) {
+            Log::error('Failed syncing avatar/details for ' . $this->friend->username . '; exception: ' . $e->getMessage());
             // No big deal, as we have a default avatar
         }
 
+        Log::error('Failed syncing avatar/details for ' . $this->friend->username);
         return false;
     }
 }
