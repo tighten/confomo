1c018e5058a24b849e9227a004d729c6549788b0
Laravel Shift <shift@laravelshift.com>
2017-07-10 20:12:26 +0000

    Shift bindings
    Since Laravel 5.1 requires PHP 5.5.9+ using the *new* static `class`
    property to get the fully qualified name of a class is preferred
    over using class name strings.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\FriendWasAdded' => [
-            'App\Listeners\FetchFriendAvatar',
+        \App\Events\FriendWasAdded::class => [
+            \App\Listeners\FetchFriendAvatar::class,
         ],
     ];
 
